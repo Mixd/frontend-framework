55b500812b0af42cf3dd130f396a58bb5b15b353
lukewhitehouse <luke@lukewhitehouse.co.uk>
2015-05-26 22:01:57 +0100

    remove https from jquery URL

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="assets/dist/js/min/main.min.js"></script>
 </body>
 </html>
