4d93b762c4050dac2f0abd23cdd76bb49717d481
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2015-05-28 16:59:44 +0100

    update svg symbols + testing

--- index.php
+++ index.php
@@ -5,8 +5,4 @@
     <p>Welcome to the Mixd Front-end Framework.</p>
 </div>
 
-<svg viewBox="0 0 100 100">
-  <use xlink:href="#arrow-right-invert"></use>
-</svg>
-
 <?php include('includes/footer.php'); ?>
