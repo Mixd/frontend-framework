ba2fe3d51315bf257cb73634283a1d8cfa79a751
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2015-05-29 16:12:02 +0100

    update fallback

--- assets/app/js/partials/icons-fallback.js
+++ assets/app/js/partials/icons-fallback.js
@@ -1,67 +1,88 @@
-// Fallback for old browsers with the indexOf() function
-if (!Array.prototype.indexOf) {
-    Array.prototype.indexOf = function(elt /*, from*/) {
-        var len = this.length >>> 0;
+// Test for Inline SVG support
+function supportsSvg() {
+    var div = document.createElement('div');
+    div.innerHTML = '<svg/>';
+    return (div.firstChild && div.firstChild.namespaceURI) == 'http://www.w3.org/2000/svg';
+};
 
-        var from = Number(arguments[1]) || 0;
-        from = (from < 0)
-            ? Math.ceil(from)
-            : Math.floor(from);
-        if (from < 0)
-            from += len;
+// Scope everything into a SIAF
+(function(){
 
-        for (; from < len; from++) {
-            if (from in this && this[from] === elt)
-                return from;
-        }
-        return -1;
-    };
-}
+    // If browser doesn't support Inline SVG
+    if ( !supportsSvg() ) {
 
-// Get all SVGs on the page
-var svgs = document.getElementsByTagName("svg"),
-    svgL = svgs.length;
+        // Get all SVGs on the page and how many there are
+        var svgs = document.getElementsByTagName("svg"),
+            svgL = svgs.length;
 
-// Loop through all SVGs on the page
-while( svgL-- ) {
+        // Loop through all SVGs on the page
+        while( svgL-- ) {
 
-    // If SVG doesn't have class, continue ...
-    if(svgs[svgL].className.baseVal.indexOf('nopng') < 0) {
+            // Define class check variable
+            var checkSvgClass;
 
-        // If this is not the first SVG, continue ...
-        if(svgL > 0) {
+            // Check if browser supports indexOf() function
+            if ('indexOf' in Array.prototype) {
 
-            // Get title attribute of SVG
-            var svgTitle = svgs[svgL].getAttribute("title");
+                // use native indexOf
+                checkSvgClass = svgs[svgL].className.baseVal.indexOf('nopng');
+            } else {
 
-            // Get all <use> elements from each SVG
-            var uses = svgs[svgL].getElementsByTagName("use"),
-                usesL = uses.length;
+                // create custom indexOf fallback function
+                function indexOfFallback(elem, find, i) {
+                    if (i===undefined) i= 0;
+                    if (i<0) i+= this.length;
+                    if (i<0) i= 0;
+                    for (var n= this.length; i<n; i++)
+                        if (i in this && this[i]===find)
+                            return i;
+                    return -1;
+                };
 
-            // Loop through all <use> elements within an SVG
-            while( usesL-- ) {
+                // use fallback
+                checkSvgClass = indexOfFallback(svgs[svgL].className.baseVal, 'nopng');
+            }
 
-                // Get the 'xlink:href' attributes
-                var svgId = uses[usesL].getAttribute("xlink:href");
+            // If SVG doesn't have class, continue ...
+            if(checkSvgClass < 0) {
 
-                // Remove first character from variable (This removes the #)
-                svgId = svgId.substring(1, svgId.length);
+                // If this is not the first SVG, continue ...
+                if(svgL > 0) {
 
-                // Create New Image
-                var newImg = document.createElement("img");
+                    // Get title attribute of SVG
+                    var svgTitle = svgs[svgL].getAttribute("title");
 
-                // Assign src attribute
-                newImg.src = "assets/dist/icons/png/" + svgId + ".png";
+                    // Get all <use> elements from each SVG
+                    var uses = svgs[svgL].getElementsByTagName("use"),
+                        usesL = uses.length;
 
-                // Assign alt attribute
-                newImg.alt = svgTitle ? svgTitle : '';
+                    // Loop through all <use> elements within an SVG
+                    while( usesL-- ) {
 
-                // Insert new element straight after the SVG in question
-                svgs[svgL].parentNode.insertBefore(newImg, svgs[svgL].nextSibling);
+                        // Get the 'xlink:href' attributes
+                        var svgId = uses[usesL].getAttribute("xlink:href");
+
+                        // Remove first character from variable (This removes the #)
+                        svgId = svgId.substring(1, svgId.length);
+
+                        // Create New Image
+                        var newImg = document.createElement("img");
+
+                        // Assign src attribute
+                        newImg.src = "assets/dist/icons/png/" + svgId + ".png";
+
+                        // Assign alt attribute
+                        newImg.alt = svgTitle ? svgTitle : '';
+
+                        // Insert new element straight after the SVG in question
+                        svgs[svgL].parentNode.insertBefore(newImg, svgs[svgL].nextSibling);
+                    }
+                }
+
+                // Remove all SVG nodes
+                svgs[svgL].parentNode.removeChild(svgs[svgL]);
             }
         }
-
-            // Remove all SVG nodes
-            svgs[svgL].parentNode.removeChild(svgs[svgL]);
     }
-}
+
+})();
