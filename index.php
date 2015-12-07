371ad71274e13f74b0067da8cb512c67f11ebfe3
lukewhitehouse <luke@lukewhitehouse.co.uk>
2015-12-07 21:54:46 +0000

    remove unused stuff

--- index.php
+++ index.php
@@ -3,10 +3,6 @@
 <div class="container">
     <h1>Hello World</h1>
     <p>Welcome to the Mixd Front-end Framework.</p>
-
-    <svg viewBox="0 0 100 100" role="presentation">
-        <use xlink:href="#logo-mixd"></use>
-    </svg>
 </div>
 
 <?php include('includes/footer.php'); ?>
