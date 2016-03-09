dff242ca56fe17d3d18d1a4d192df2829e032e8c
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2016-03-09 15:42:36 +0000

    make sure styles.css has correct name when compiled

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <div class="container">
     <h1>Hello World</h1>
     <p>Welcome to the Mixd Front-end Framework.</p>
-    <span class="icon icon--logo-mixd"></span>
+    <span class="icon icon--large icon--logo-mixd"></span>
 </div>
 
 <?php include('includes/footer.php'); ?>
