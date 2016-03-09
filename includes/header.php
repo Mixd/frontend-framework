1e6fd6278c1c717e83ebdb9e96dfe544be5f27bf
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2016-03-09 14:09:21 +0000

    add comment references

--- includes/header.php
+++ includes/header.php
@@ -2,17 +2,20 @@
 <!--[if lt IE 9]><html class="no-js ie lt-ie9 " lang="en"><![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!--><html class="no-js " lang="en"><!--<![endif]-->
 <head>
+    <!-- Meta Data -->
     <meta charset="UTF-8">
     <meta http-equiv="cleartype" content="on">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Titles/Descriptions -->
     <title>Mixd Front-end Framework</title>
-    <meta name="description" content="">
 
+    <!-- Favicons -->
     <link rel="shortcut icon" href="/icon.png">
     <link rel="apple-touch-icon-precomposed" href="/icon.png">
 
+    <!-- Styles -->
     <!--[if IE 9]><!-->
     <link rel="stylesheet" href="/assets/dist/css/styles.css">
     <!--<![endif]-->
@@ -21,8 +24,11 @@
         <link rel="stylesheet" href="/assets/dist/css/ie.css" media="screen">
     <![endif]-->
 
-    <noscript><link href="/assets/dist/grunticon/icons.fallback.css" rel="stylesheet"></noscript>
+    <!-- @font-face declarations -->
+    <!-- insert here -->
 
+    <!-- Scripts -->
+    <noscript><link href="/assets/dist/grunticon/icons.fallback.css" rel="stylesheet"></noscript>
     <script src="/assets/dist/js/head.min.js"></script>
 </head>
 <body class="debug">
